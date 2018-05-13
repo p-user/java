1eebc6d3ae4035c3fe81bbeb9f9dcfa455c219bb
pammy <pamelaagaj@gmail.com>
2018-05-13 23:34:14 +0000

    Delete TestingLabel.java

--- TestingLabel.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JFrame;
-public class TestingLabel {
-	public static void main(String[] args) {
-		LabelFrame labelFrame=new LabelFrame();
-		labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		labelFrame.setSize(300,180);
-		labelFrame.setVisible(true);
-	}
-}
